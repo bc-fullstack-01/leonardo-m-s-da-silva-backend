ead97876576962892ad9648e9d5bdb9682b8cc3b
LeoMSSilva <l.eom1994@hotmail.com>
2022-07-01 22:11:51 -0300

    Feat: modified server to render the page using path and fs modules

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
+import fs from 'fs';
+import path from 'path';
 import express from 'express';
 const app = express();
 
 const port = 4000;
 
-app.get('/', (req, res) => res.send('Hello from app!'));
+app.get('/', (req, res) => {
+  const __dirname = path.resolve('src');
+  const pathDir = path.join(__dirname, 'static', 'index.html');
+  const content = fs.readFileSync(pathDir, 'utf-8');
+  res.send(content);
+});
 
 app.listen(port, () =>
   console.log(`Server listen on http://localhost:${port}`)
